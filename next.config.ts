da6fd3f4662d32d9ab930a0b0887eab1278b7b57
Lotfi Najim <lotfi.najim@outlook.com>
2025-01-06 20:41:22 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedOrigins: ["104.248.131.56:8080"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["104.248.131.56:8080"],
+    },
+  },
 };
 
 export default nextConfig;
